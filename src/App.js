0d5f429e1aab8f96b38682a86823fbd1cf259a6e
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-06-02 19:45:32 -0300

    api with axios:github

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import api from './services/api'
+
+import "./global.css"
 
 export default function App() {
   const [user, setUser] = useState();
 
+  useEffect(() => {
+    api
+    .get("/users/luisdasilvahenrique")
+    .then((response) => setUser(response.data))
+    .catch((err) => {
+      console.log("ops! ocorreu um erro" + err);
+    })
+  }, []);
+  
+
   return (
     <div className="App">
-      <p>Usuário: {user.login}</p>
-      <p> Biografia: {user.bio}</p>
+      <p> <strong>Usuário</strong> : {user?.login}</p>
+      <p> <strong>Biografia</strong> : {user?.bio}</p>
     </div>
   );
 }
